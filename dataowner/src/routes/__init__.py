41157a262bd18d9a870e7859533505291b54bdfb
ShanelReyes <shanelreyesp@gmail.com>
2023-05-23 23:57:36 -0600

    changes in dataowner

--- dataowner/src/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-from pathlib import Path
-path_root      = Path(__file__).parent.absolute()
-(path_root, _) = os.path.split(path_root)
-newPath = str(path_root) +"/src"
-print(newPath)
-sys.path.append(newPath)
