7e5d213d6076d5254f867548c1c1a1f5d92e74ae
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2025-04-13 09:38:38 -0600

    Rorycommon tests suites

--- rorycommon/tests/test_put_get_matrix.py
+++ rorycommon/tests/test_put_get_matrix.py
@@ -1,13 +1,27 @@
+import hashlib as H
+import time as T
+import asyncio
 import pytest
 from rorycommon import Common as RoryCommon
 import os 
+import pickle as PK
 import numpy as np
+from typing import List,Tuple
+from option import Result,Some,NONE
 from mictlanx import AsyncClient
 from mictlanx.utils import Utils
+from mictlanx.utils.segmentation import Chunks
 from concurrent.futures import ProcessPoolExecutor
 from rory.core.security.dataowner import DataOwner
+from rory.core.security.pqc.dataowner import DataOwner as DataOwnerPQC
 from rory.core.security.cryptosystem.liu import Liu
+from Pyfhel import PyCtxt
+from rory.core.utils.constants import Constants
+import numpy.typing as npt
 from xolo.utils.utils import Utils as XoloUtils
+from rory.core.security.cryptosystem.pqc.ckks import Ckks
+from rory.core.clustering.secure.pqc.skmeans import Skmeans as SkmeansPQC
+
 MICTLANX_CLIENT_ID           = os.environ.get("MICTLANX_CLIENT_ID","{}_mictlanx".format("rory-common"))
 MICTLANX_TIMEOUT             = int(os.environ.get("MICTLANX_TIMEOUT",3600))
 MICTLANX_API_VERSION         = int(os.environ.get("MICTLANX_API_VERSION","3"))
@@ -41,7 +55,8 @@ dataowner = DataOwner(
     ),
 )
 
-key = "encryptedskmeansy"
+# key = "encryptedskmeansy"
+key = "encryptedskmeanspqc1"
 bucket_id = "rory"
 @pytest.mark.skip("")
 @pytest.mark.asyncio
@@ -96,8 +111,295 @@ async def test_get_and_merge():
         key = key, 
         bucket_id = bucket_id
     )
+
+
+
+@pytest.mark.skip("")
+@pytest.mark.asyncio
+async def test_put_pqx():
+    pmt_result = await RoryCommon.read_numpy_from(
+        path="/rory/source/clusteringc0r10a5k20.npy",
+        extension="npy"
+    )
+    assert pmt_result.is_ok
+    pmt = pmt_result.unwrap()
+
+    print(pmt)
+    n          = pmt.shape[1]*pmt.shape[1]
+    num_chunks = 2
+    key = "xxx"
+    emt = RoryCommon.segment_and_encrypt_ckks_with_executor(
+        executor         = ProcessPoolExecutor(max_workers=num_chunks),
+        key              = key,
+        plaintext_matrix = pmt,
+        n                = n,
+        num_chunks       = num_chunks,
+        _round = False,
+        ctx_filename="ctx", 
+        pubkey_filename="pubkey",
+        secretkey_filename="secretkey",
+        path="/rory/keys",
+        decimals=2
+    ) 
+    res = await RoryCommon.delete_and_put_chunks(client=client, bucket_id=bucket_id,key="xxx", chunks=emt, tags={
+        "x":"Something_test"
+    })
+    assert res.is_ok
+
+@pytest.mark.skip("")
+@pytest.mark.asyncio
+async def test_get_pqx():
+    # key = "encryptedskmeansy"
+    # bucket_id = "rory"
+    ckks = Ckks.from_pyfhel(
+        path="/rory/keys",
+    )
+    print(ckks.he_object)
+    # print(ckks.he_object)
+    key ="xxx"
+    x = await RoryCommon.get_pyctxt(
+        client = client, 
+        key = key, 
+        bucket_id = bucket_id,
+        ckks=ckks
+    )
+    print(x,len(x))
+    print("="*50)
+    maybe_chs = RoryCommon.from_pyctxts_to_chunks(key=key,xs=x)
+    assert maybe_chs.is_some
+    chs = maybe_chs.unwrap()
+    xx = RoryCommon.from_chunks_to_pyctxts_list(ckks = ckks, chunks=chs)
+    print(xx,len(xx))
+    tmp = np.array(ckks.decrypt_list(xs = xx, take=5)).reshape((10,5))
+    print(tmp)
+    # for e in xx:
+    #     # tmp = ckks.he_object.decrypt(e)[:5
+    #     tmp = ckks.decrypt_list()
+    #     print(tmp)
+    # xxx = ckks.decrypt_list(xs = xx)
+    # xxx = ckks.decryptVe2ctor(np.array(xx))
+    # print(xxx,len(xxx))
+    # for c in chs:
+    #     x = c.to_list().unwrap()
+    #     xx = RoryCommon.bytes_to_pyctxt_list(ckks=ckks, serialized_ctxt_bytes=x)
+
+    
     # xx = dataowner.liu_scheme.decryptMatrix(x)
     # print(xx)
     
     # print(x)
+    # <ckks Pyfhel obj at 0x7fa556cd1160, [pk:Y, sk:Y, rtk:-, rlk:-, contx(n=16384, t=0, sec=128, qi=[60, 40, 40, 40, 60], scale=1099511627776.0, )]>
+
+
+@pytest.mark.skip("")
+@pytest.mark.asyncio
+async def test_shift_matrix():
+    executor = ProcessPoolExecutor(max_workers=2)
+    init_sm_id = "initsmid"
+    num_chunks = 2
+    _round = False
+    decimals = 2
+    path = "/rory/keys"
+    ctx_filename = "ctx"
+    pubkey_filename = "pubkey"
+    secretkey_filename = "secretkey"
+    ckks = Ckks.from_pyfhel(
+        path="/rory/keys",
+    )
+    dataowner = DataOwnerPQC(scheme = ckks)  ##
+
+    BUCKET_ID = "rory"
+
+    encrypted_matrix_id = "encryptedmatrixid"
+    # plaintext_matrix = None 
+    extension = "npy"
+    plaintext_matrix_path = f"/rory/source/clusteringc0r10a5k20.{extension}"
+    plaintext_matrix  = (await RoryCommon.read_numpy_from(
+        path      = plaintext_matrix_path,
+        extension = extension,
+    )).unwrap()
+    r = plaintext_matrix.shape[0]
+    a = plaintext_matrix.shape[1]
+    n = a*r
+    k = 2
+
+    print("*"*50)
+    encrypted_matrix_chunks = RoryCommon.segment_and_encrypt_ckks_with_executor( #Encrypt 
+        executor           = executor,
+        key                = encrypted_matrix_id,
+        plaintext_matrix   = plaintext_matrix,
+        n                  = n,
+        _round             = _round,
+        decimals           = decimals,
+        path               = path,
+        ctx_filename       = ctx_filename,
+        pubkey_filename    = pubkey_filename,
+        secretkey_filename = secretkey_filename,
+        num_chunks         = num_chunks,
+    )
+    put_encrypted_matrix_result = await RoryCommon.delete_and_put_chunks(
+        client = client,
+        bucket_id      = BUCKET_ID,
+        key            = encrypted_matrix_id,
+        chunks         = encrypted_matrix_chunks,
+        tags = {
+            "full_shape": str((r,a)),
+            "full_dtype":"float64"
+        }
+    )
+    print("PUT_EM", put_encrypted_matrix_result)
+
+    # ===================================================
+    zero_shiftmatrix = np.zeros((k, a))
+    n2 = a*k
+    print("___"*20)
+    encrypted_zero_shiftmatrix_chunks = RoryCommon.segment_and_encrypt_ckks_with_executor( #Encrypt 
+        executor           = executor,
+        key                = init_sm_id,
+        plaintext_matrix   = zero_shiftmatrix,
+        n                  = n2,
+        num_chunks         = num_chunks,
+        _round             = _round,
+        decimals           = decimals,
+        path               = path,
+        ctx_filename       = ctx_filename,
+        pubkey_filename    = pubkey_filename,
+        secretkey_filename = secretkey_filename
+    )
+
+ 
+    put_encrypted_zero_shiftmatrix_result = await RoryCommon.delete_and_put_chunks(
+        client         = client,
+        bucket_id      = BUCKET_ID,
+        key            = init_sm_id,
+        chunks         = encrypted_zero_shiftmatrix_chunks,
+        tags = {
+            "full_shape": str((k,a)),
+            "full_dtype":"float64"
+        }
+    )
+    print("PUT_SM", put_encrypted_zero_shiftmatrix_result)
+    print("__"*20)
+    # # ===================================================
+    udm            = dataowner.get_U(
+        plaintext_matrix = plaintext_matrix,
+        algorithm        = Constants.ClusteringAlgorithms.SKMEANS_PQC
+    )
+    udm_id="udmid"
+
+    maybe_udm_matrix_chunks = Chunks.from_ndarray(
+        ndarray      = udm,
+        group_id     = udm_id,
+        chunk_prefix = Some(udm_id),
+        num_chunks   = num_chunks,
+    )
+
+
+    udm_put_result = await RoryCommon.delete_and_put_chunks(
+        client         = client,
+        bucket_id      = BUCKET_ID,
+        key            = udm_id,
+        chunks         = maybe_udm_matrix_chunks.unwrap(),
+        tags = {
+            "full_shape": str(udm.shape),
+            "full_dtype": str(udm.dtype)
+        }
+    )
+    print("PUT_UDM", udm_put_result)
+    print("__"*20)
+    # # ===================================================
+    init_shiftmatrix = await RoryCommon.get_pyctxt(
+            client = client, 
+            bucket_id=BUCKET_ID, 
+            key=init_sm_id, 
+            ckks = ckks,
+    )
+    print(init_shiftmatrix)
+    print("__"*20)
+    skmeans = SkmeansPQC(he_object=ckks.he_object, init_shiftmatrix=init_shiftmatrix)
+    _encryptedMatrix = await RoryCommon.get_pyctxt(
+        client    = client,
+        bucket_id = BUCKET_ID,
+        key       = encrypted_matrix_id,
+        ckks      = ckks
+    )
+    print(_encryptedMatrix)
+    print("__"*20)
+    _udm = await RoryCommon.get_and_merge(
+        client    = client,
+        bucket_id = BUCKET_ID,
+        key       = udm_id,
+    )
+    S1,_Cent_i,_Cent_j, label_vector  = skmeans.run1(
+        status= Constants.ClusteringStatus.WORK_IN_PROGRESS, 
+        k = k, 
+        encryptedMatrix= _encryptedMatrix, 
+        Cent_j=init_shiftmatrix,
+        num_attributes=plaintext_matrix.shape[1],
+        UDM=_udm,
+    ).unwrap()
+
+    print("S1",S1)
+    print("Ci",_Cent_i)
+    print("Cj",_Cent_j)
+    cent_i_id = "centiid"
+    cent_j_id = "centjid"
+    encrypted_shift_matrix_id = "encryptedshiftmatrixid"
+    # cent_i_chunks                 = RoryCommon.from_pyctxts_to_chunks(key=cent_i_id,xs= _Cent_i,num_chunks=num_chunks).unwrap()
+    # cent_j_chunks                 = RoryCommon.from_pyctxts_to_chunks(key=cent_j_id,xs= _Cent_j,num_chunks=num_chunks).unwrap()
+    encrypted_shift_matrix_chunks = RoryCommon.from_pyctxts_to_chunks(key=encrypted_shift_matrix_id, xs = S1,num_chunks=num_chunks).unwrap()
     
+    z = await RoryCommon.delete_and_put_chunks(
+        client = client,
+        bucket_id      = BUCKET_ID,
+        key            = encrypted_shift_matrix_id,
+        chunks         = encrypted_shift_matrix_chunks
+    )
+    print(z)
+    # await asyncio.sleep(5)
+    zz = await RoryCommon.get_pyctxt(
+        client=client, 
+        bucket_id=BUCKET_ID,
+        key=encrypted_shift_matrix_id,
+        ckks=ckks,
+        delay=2,
+        max_retries=20
+    )
+    print(zz)
+
+    # print(skmeans)
+    # status = 0
+    # encryptedMatrix = None
+    # udm = None
+    # __Cent_j = None
+
+    # num_attrs = 0
+    # run1_result:Result[Tuple[npt.NDArray, List[PyCtxt], List[PyCtxt], List[int]],Exception] = skmeans.run1( # The first part of the skmeans is done
+    #         status          = status,
+    #         k               = k,
+    #         encryptedMatrix = encryptedMatrix, 
+    #         UDM             = udm,
+    #         Cent_j          = __Cent_j,
+    #         num_attributes  = num_attrs
+    # )
+
+@pytest.mark.skip("")
+@pytest.mark.asyncio
+async def test_get_encrypted_shift_matrix():
+    encrypted_shift_matrix_id = "encryptedshiftmatrixid"
+    BUCKET_ID = "rory"
+    
+    ckks = Ckks.from_pyfhel(
+        path="/rory/keys",
+    )
+
+    zz = await RoryCommon.get_pyctxt(
+        client=client, 
+        bucket_id=BUCKET_ID,
+        key=encrypted_shift_matrix_id,
+        ckks=ckks,
+        delay=2,
+        max_retries=20,
+        force=True
+    )
+    print(zz)
--- rorycommon/tests/test_segment_encrypt.py
+++ rorycommon/tests/test_segment_encrypt.py
@@ -8,6 +8,7 @@ from concurrent.futures import ProcessPoolExecutor
 from rory.core.security.dataowner import DataOwner
 from rory.core.security.cryptosystem.liu import Liu
 from xolo.utils.utils import Utils as XoloUtils
+from rory.core.security.cryptosystem.pqc.ckks import Ckks
 dataowner = DataOwner(
     liu_scheme= Liu(
         _round         = True,
@@ -47,7 +48,9 @@ async def test_liu():
 
     
 
-# @pytest.mark.skip("")
+
+
+@pytest.mark.skip("")
 @pytest.mark.asyncio
 async def test_fdhope():
     pmt_result = await RoryCommon.read_numpy_from(
@@ -80,4 +83,46 @@ async def test_fdhope():
     for c in emt:
         print(c)
 
-    
+@pytest.mark.skip("")
+@pytest.mark.asyncio
+async def test_generate_ckks_keys():
+    x = Ckks.create_client(
+        scheme="CKKS",
+        n=8192,
+        scale=2**30,
+        qi_sizes=[60,40,40,60],
+        decimals=2,
+        save=True,
+        output_path="/rory/keys"
+
+    )
+    x.he_object.res
+    print(x)
+@pytest.mark.skip("")
+@pytest.mark.asyncio
+async def test_ckks():
+    pmt_result = await RoryCommon.read_numpy_from(
+        path="/rory/source/clusteringc0r10a5k20.npy",
+        extension="npy"
+    )
+    assert pmt_result.is_ok
+    pmt = pmt_result.unwrap()
+
+    n          = pmt.shape[1]*pmt.shape[1]
+    num_chunks = 2
+    emt = RoryCommon.segment_and_encrypt_ckks_with_executor(
+        executor         = ProcessPoolExecutor(max_workers=num_chunks),
+        key              = key,
+        plaintext_matrix = pmt,
+        n                = n,
+        num_chunks       = num_chunks,
+        _round = False,
+        ctx_filename="ctx", 
+        pubkey_filename="pubkey",
+        secretkey_filename="secretkey",
+        path="/rory/keys",
+        decimals=2
+    )
+    for c in emt:
+        print(c)
+
